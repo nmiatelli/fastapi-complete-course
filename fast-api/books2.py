5d8cfb1d544a499122217897a353baeb3b794690
Natalia Miatelli <nataliamiatelli@gmail.com>
2026-04-29 14:58:31 -0300

    Created validation with Query and Path

--- fast-api/books2.py
+++ fast-api/books2.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import  FastAPI
+from fastapi import  FastAPI, Path, Query
 from pydantic import BaseModel, Field
 
 app = FastAPI()
@@ -58,13 +58,13 @@ async def read_all_books():
     return BOOKS
 
 @app.get("/books/{book_id}")
-async def read_book(book_id: int):
+async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
         
 @app.get("/books/")
-async def read_book_by_rating(book_rating: int):
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
     books_to_return = []
     for book in BOOKS:
         if book.rating == book_rating:
@@ -72,12 +72,12 @@ async def read_book_by_rating(book_rating: int):
     return books_to_return
 
 @app.get("/books/publish/")
-async def read_book_by_publish_date(published_date: int):
+async def read_book_by_publish_date(published_date: int = Query(gt=1999, lt=2031)):
     books_by_date=[]
     for book in BOOKS:
         if book.published_date == published_date:
             books_by_date.append(book)
-            
+
     return books_by_date
     
 
@@ -104,7 +104,7 @@ async def update_book(book: BookRequest):
            BOOKS[i] = book # type: ignore
 
 @app.delete("/book/{book_id}")
-async def delete_book(book_id: int):
+async def delete_book(book_id: int = Path(gt=0)):
     for i in range (len(BOOKS)):
         if BOOKS[i].id == book_id:
             BOOKS.pop(i)
